62d0dc197bd1357764a8d93c18e24522088ed7bb
Snack Man <lpardo1989@gmail.com>
2023-07-02 10:14:53 -0700

    package json file

--- Develop/public/server.js
+++ Develop/public/server.js
@@ -3,3 +3,10 @@ const app = express();
 const PORT = process.env.port || 3001;
 
 app.use(express.static('public'));
+
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/')));
+
+app.listen(PORT, () =>
+  console.log(`Serving static asset routes at http://localhost:${PORT}!`)
+);
